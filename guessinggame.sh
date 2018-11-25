648cd6955b25c624a5425a8f8f19eae1db90cec3
TravisDS1 <tschoneman369@hotmail.com>
2018-11-25 12:54:20 -0600

    updated code to be a function guessinggame in guessingame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-count=$(ls -al | grep ^[-] | wc -l)
-echo "How many files would you guess are in the current directoy? Type a guess and hit Enter:"
-read guess
-while [[ $guess -ne $count ]]
-do
-  if [[ $guess -lt $count ]]
-  then
-    echo "That is too low. Guess again:"
-    read next_guess
-    guess=$next_guess
-  else
-    echo "That is too high. Guess again:"
-    read next_guess
-    guess=$next_guess
-  fi
-done
-echo "Congratulations! You guessed right."
+function guessinggame {
+  count=$(ls -al | grep ^[-] | wc -l)
+  echo "How many files would you guess are in the current directory? Type a guess and hit Enter:"
+  read guess
+  while [[ $guess -ne $count ]]
+  do
+    if [[ $guess -lt $count ]]
+    then
+      echo "That is too low. Guess again:"
+      read next_guess
+      guess=$next_guess
+    else
+      echo "That is too high. Guess again:"
+      read next_guess
+      guess=$next_guess
+    fi
+  done
+  echo "Congratulations! You guessed right."
+}
+
+guessinggame   # call function guessingame to run
